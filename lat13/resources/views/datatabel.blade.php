4fe7909d651630566d9880cf5091c51bc76c2418
suryan <suryan@torche.co.id>
2020-08-06 10:27:06 +0700

    revisi css datatables

--- lat13/resources/views/datatabel.blade.php
+++ lat13/resources/views/datatabel.blade.php
@@ -443,8 +443,9 @@
 @endsection
 
 @push('script')
-    <script src="../../plugins/datatables/jquery.dataTables.js"></script>
-    <script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+    <link rel="stylesheet" href="{{url('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+    <script src="{{'plugins/datatables/jquery.dataTables.js'}}"></script>
+    <script src="{{'plugins/datatables-bs4/js/dataTables.bootstrap4.js'}}"></script>
     <script>
         $(function () {
             $("#example1").DataTable();
